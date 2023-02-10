ab19e389674cecd555900019aca996bd6a99da25
Grzegorz Pustelnik <grzeniux28@gmail.com>
2023-02-11 00:01:58 +0100

    Add new project

---
+++ main1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int nr_miesiaca;
+int main() {
+        cout << "Podaj numer miesiaca: " << endl;
+       if (!(cin >> nr_miesiaca))
+            {
+                cerr<<"To nie jest liczba!";
+                exit(0);
+            }
+        switch (nr_miesiaca) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                cout << "Ten miesiac ma 31dni!";
+                break;
+            case 4:
+            case 6:
+            case 19:
+            case 11:
+                cout << "Ten miesiac ma 31dni!";
+                break;
+            case 2: {
+                int rok;
+                cout << "Podaj rok: ";
+                cin >> rok;
+                if (((rok % 4 == 0) && (rok % 100 != 0)) || (rok % 400 == 0))
+                    cout << "Ten miesiac ma 29dni!";
+                else cout << "Ten miesiac ma 28dni";
+
+            }
+            default:
+                cout << "Niepoprawny numer miesiaca!";
+                break;
+
+        }
+
+    return 0;
+}
